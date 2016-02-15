3e1180b6020d50154a3356b8d2399f8a8b2965ce
Nam Nguyen <ntgn81@gmail.com>
2016-02-15 00:45:27 -0500

    First commit

---
+++ src/Utils.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Utils.API
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            // Make json the only and default response
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Camel-casing json response
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate;
+
+            // Enable CORS
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+        }
+    }
+}
---
+++ src/Utils.API/Controllers/PdfController.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using System.Linq;
+using System.IO;
+using System.Net.Http.Headers;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace Utils.API.Controllers
+{
+    [RoutePrefix("pdf")]
+    public class PdfController : ApiController
+    {
+        [HttpGet, Route("merge")]
+        public HttpResponseMessage Merge(string urls)
+        {
+            var urlArray = urls.Split(',');
+
+            var wc = new WebClient();
+            var pdfContents = urlArray.Select(u => this.Download(wc, u)).ToList();
+            byte[] mergedPdf = null;
+            using (var ms = new MemoryStream())
+            {
+                using (var document = new Document())
+                {
+                    using (var copy = new PdfCopy(document, ms))
+                    {
+                        document.Open();                 
+                        pdfContents.ForEach(c => {
+                            if (c != null) copy.AddDocument(new PdfReader(c));
+                        });
+                    }
+                }
+                mergedPdf = ms.ToArray();
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ByteArrayContent(mergedPdf);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+            return response;
+        }
+
+        private byte[] Download(WebClient wc, string url)
+        {
+            try
+            {
+                return wc.DownloadData(url);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
