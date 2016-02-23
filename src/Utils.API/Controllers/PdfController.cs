da1bc7bdd258dfdd17cd968d095c11a20f5fa687
Nam Nguyen <ntgn81@gmail.com>
2016-02-23 14:09:48 -0500

    Form filing

--- src/Utils.API/Controllers/PdfController.cs
+++ src/Utils.API/Controllers/PdfController.cs
@@ -6,6 +6,10 @@ using System.IO;
 using System.Net.Http.Headers;
 using iTextSharp.text.pdf;
 using iTextSharp.text;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System;
 
 namespace Utils.API.Controllers
 {
@@ -18,7 +22,7 @@ namespace Utils.API.Controllers
             var urlArray = urls.Split(',');
 
             var wc = new WebClient();
-            var pdfContents = urlArray.Select(u => this.Download(wc, u)).ToList();
+            var pdfContents = urlArray.Select(u => Download(wc, u)).ToList();
             byte[] mergedPdf = null;
             using (var ms = new MemoryStream())
             {
@@ -34,11 +38,58 @@ namespace Utils.API.Controllers
                 }
                 mergedPdf = ms.ToArray();
             }
-
             var response = new HttpResponseMessage(HttpStatusCode.OK);
             response.Content = new ByteArrayContent(mergedPdf);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
             return response;
+        }
+
+        [HttpGet, Route("fill-form")]
+        public HttpResponseMessage FillForm(string formFile, string dataUrl)
+        {
+            var file = formFile.Contains(".pdf") ? formFile : $"{formFile}.pdf";
+            var form = HttpContext.Current.Request.MapPath($"~\\App_Data\\Forms\\{file}");
+
+            byte[] filedPdf;
+            using (var fileStream = File.OpenRead(form))
+            {
+                using (var outStream = new MemoryStream())
+                {
+                    XFAImport(fileStream, Download(new WebClient(), dataUrl), outStream);
+                    filedPdf = outStream.ToArray();
+                }
+            }
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ByteArrayContent(filedPdf);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+            return response;
+        }
+
+        [HttpPost, Route("fill-form")]
+        public HttpResponseMessage FillFormPost(string formFile)
+        {
+            var file = formFile.Contains(".pdf") ? formFile : $"{formFile}.pdf";
+            var form = HttpContext.Current.Request.MapPath($"~\\App_Data\\Forms\\{file}");
+            var dataXml = GetBody(Request).Result;
+
+            byte[] filedPdf;
+            using (var fileStream = File.OpenRead(form))
+            {
+                using (var outStream = new MemoryStream())
+                {
+                    XFAImport(fileStream, Encoding.UTF8.GetBytes(dataXml), outStream);
+                    filedPdf = outStream.ToArray();
+                }
+            }
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ByteArrayContent(filedPdf);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+            return response;
+        }
+
+        private async Task<string> GetBody(HttpRequestMessage request)
+        {
+            return await request.Content.ReadAsStringAsync();
         }
 
         private byte[] Download(WebClient wc, string url)
@@ -50,6 +101,27 @@ namespace Utils.API.Controllers
             catch
             {
                 return null;
+            }
+        }
+
+        /// <summary>
+        /// Imports XFA Data into a new PDF file.
+        /// </summary>
+        /// <param name="pdfTemplate">A PDF File with an unpopulated form.</param>
+        /// <param name="xmlFormData">XFA form data in XML format.</param>
+        /// <returns>a memorystream containing the new PDF file.</returns>
+        private void XFAImport(Stream pdfTemplate, byte[] xmlFormData, Stream outputStream)
+        {
+            using (var reader = new PdfReader(pdfTemplate))
+            {
+                using (var stamper = new PdfStamper(reader, outputStream, '\0', true))
+                {
+                    using (var ms = new MemoryStream(xmlFormData))
+                    {
+                        stamper.Writer.CloseStream = false;
+                        stamper.AcroFields.Xfa.FillXfaForm(ms);
+                    }
+                }
             }
         }
     }
